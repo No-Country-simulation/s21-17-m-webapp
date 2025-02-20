4ec281d8c08b20c9ffead61baca19c47d5b690d3
Comagol <comabellafrancisco@gmail.com>
2025-02-20 00:22:30 -0300

    adding Route to cart and creatting cart.jsx

--- client/src/app/routes/routes.jsx
+++ client/src/app/routes/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, RouterProvider } from 'react-router-dom';
 import Auth from '../../features/auth/pages/home';
+import Cart from '../../features/cart/pages/Cart';
 
 /* 
     Se debe importar cada pagina y agregarlo dentro de <routes>, 
@@ -11,7 +12,7 @@ const AppRoutes = () => {
 
       <Routes>
         <Route path='/' element={<Auth />} />
-        
+        <Route path='/cart' element={<Cart />}/>
       </Routes>
   );
 };
