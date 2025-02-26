772cb4cbc325b63415e9a0224bbca11cd5f89e7a
naylapalaoro <nayluclash@gmail.com>
2025-02-26 19:45:09 -0300

    modificacion de sesion y local storage

--- client/src/app/providers/AuthProvider.jsx
+++ client/src/app/providers/AuthProvider.jsx
@@ -4,20 +4,25 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [userType, setUserType] = useState(localStorage.getItem("userType") || null);
+  const [token, setToken] = useState(sessionStorage.getItem("token") || null);
 
   const login = (userData) => {
+    setToken(userData.token);
     setUserType(userData.user.type);
     
+    sessionStorage.setItem("token", userData.token);
     localStorage.setItem("userType", userData.user.type);
   };
 
   const logout = () => {
+    setToken(null);
     setUserType(null);
+    sessionStorage.removeItem("token");
     localStorage.removeItem("userType");
   };
 
   return (
-    <AuthContext.Provider value={{ userType, login, logout }}>
+    <AuthContext.Provider value={{ userType, token, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
