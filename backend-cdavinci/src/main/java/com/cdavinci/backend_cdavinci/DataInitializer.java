b892358f81264f3766489cf3219af9058d0cc1f4
Calebsenm <calebsenm@gmail.com>
2025-03-06 09:36:05 -0500

    Carga por defecto de datos corregida

--- backend-cdavinci/src/main/java/com/cdavinci/backend_cdavinci/DataInitializer.java
+++ backend-cdavinci/src/main/java/com/cdavinci/backend_cdavinci/DataInitializer.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
