07855a9650ad4387a7cd4098ef4d98e8d71b1388
jfpanchi <jefpanchi@gmail.com>
2025-03-12 18:53:16 -0500

    fix; 💄 Fix tables
    Fix ui tables

--- client/src/features/orders/components/SalesDetail.jsx
+++ client/src/features/orders/components/SalesDetail.jsx
@@ -56,49 +56,58 @@ export const SalesDetail = ({ productSelected }) => {
   }
 
   return (
-    <Box p={4} bg={"neutral"}>
-      <Table.Root
-        size="sm"
-        striped
-        bg={"neutral"}
-        variant="striped"
-        colorScheme="primary"
-      >
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeader>ID Venta</Table.ColumnHeader>
-            <Table.ColumnHeader>Precio</Table.ColumnHeader>
-            <Table.ColumnHeader>Cantidad</Table.ColumnHeader>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {sales.map((sale) => (
-            <Table.Row key={sale.idBuy}>
-              <Table.Cell>{sale.idBuy}</Table.Cell>
-              <Table.Cell>
-                {sale.price ? `$${sale.price.toFixed(2)}` : "N/A"}
-              </Table.Cell>
-              <Table.Cell>{sale.quantity}</Table.Cell>
+    <Box p={4} bg={"neutral"} display="flex" justifyContent="center">
+      <Table.ScrollArea borderWidth="1px" maxW="xl">
+        <Table.Root
+          size="sm"
+          striped
+          highlightOnHover
+          textAlign="center"
+          showColumnBorder
+          width={{ base: "100%", md: "100%" }}
+          bg={"neutral"}
+          variant="striped"
+          border={"1px solid #04041a"}
+        >
+          <Table.Header>
+            <Table.Row bg="primary">
+              <Table.ColumnHeader>ID Venta</Table.ColumnHeader>
+              <Table.ColumnHeader>Precio</Table.ColumnHeader>
+              <Table.ColumnHeader>Cantidad</Table.ColumnHeader>
             </Table.Row>
-          ))}
-        </Table.Body>
-        <Table.Footer>
-          <Table.Row>
-            <Table.Cell colSpan={3}>
-              Vendidos: {" "}
-              {sales.reduce((total, sale) => total + sale.quantity, 0)}
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell colSpan={3}>
-              Total de ventas: $
-              {sales
-                .reduce((total, sale) => total + sale.price * sale.quantity, 0)
-                .toFixed(2)}
-            </Table.Cell>
-          </Table.Row>
-        </Table.Footer>
-      </Table.Root>
+          </Table.Header>
+          <Table.Body>
+            {sales.map((sale) => (
+              <Table.Row key={sale.idBuy}>
+                <Table.Cell>{sale.idBuy}</Table.Cell>
+                <Table.Cell>
+                  {sale.price ? `$${sale.price.toFixed(2)}` : "N/A"}
+                </Table.Cell>
+                <Table.Cell>{sale.quantity}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+          <Table.Footer>
+            <Table.Row>
+              <Table.Cell colSpan={3}>
+                Vendidos:{" "}
+                {sales.reduce((total, sale) => total + sale.quantity, 0)}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell colSpan={3}>
+                Total de ventas: $
+                {sales
+                  .reduce(
+                    (total, sale) => total + sale.price * sale.quantity,
+                    0
+                  )
+                  .toFixed(2)}
+              </Table.Cell>
+            </Table.Row>
+          </Table.Footer>
+        </Table.Root>
+      </Table.ScrollArea>
     </Box>
   );
 };
--- client/src/features/profile/components/BuyList.jsx
+++ client/src/features/profile/components/BuyList.jsx
@@ -56,76 +56,88 @@ export const BuyList = ({ customerSelected }) => {
   }
 
   return (
-    <Box display={{ base: "block", md: "table" }} mt={2}>
-      <Table.Root
-        size="sm"
-        striped
-        bg={"neutral"}
-        variant="striped"
-        border={"1px solid #04041a"}
-      >
-        <Table.Header
-          bg="primary"
-          borderBottom={"1px solid #04041a"}
-          height={"35px"}
-          fontSize={"xl"}
+    <Box mt={2} display="flex" justifyContent="center" bg="neutral">
+      <Table.ScrollArea borderWidth="1px" maxW="xl">
+        <Table.Root
+          size="sm"
+          striped
+          highlightOnHover
+          textAlign="center"
+          showColumnBorder
+          width={{ base: "100%", md: "100%" }}
+          bg={"neutral"}
+          variant="striped"
+          border={"1px solid #04041a"}          
         >
-          <Table.Row>
-            <Table.ColumnHeader>Factura</Table.ColumnHeader>
-            <Table.ColumnHeader>Fecha de Compra</Table.ColumnHeader>
-            <Table.ColumnHeader>Monto</Table.ColumnHeader>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {buys.map((buy, index) => (
-            <Table.Row
-              key={buy.idBuy}
-              sx={{
-                backgroundColor: index % 2 === 0 ? "#ffe0b2" : "#f2f2b2",
-                borderBottom: "1px solid  #04041a",
-              }}
-            >
-              <Table.Cell>{buy.idBuy}</Table.Cell>
-              <Table.Cell>
-                {new Date(buy.buyDate).toLocaleDateString()}
-              </Table.Cell>
-              <Table.Cell>${buy.amount.toFixed(2)}</Table.Cell>
-              <Table.Cell>
-                <Table.Root>
-                  <Table.Header bg="primary" borderBottom={"1px solid #04041a"}>
-                    <Table.Row>
-                      <Table.ColumnHeader>Producto</Table.ColumnHeader>
-                      <Table.ColumnHeader>Precio</Table.ColumnHeader>
-                    </Table.Row>
-                  </Table.Header>
-                  <Table.Body>
-                    {buy.purchasedProducts.map((product) => (
-                      <Table.Row
-                        key={product.idProduct}
-                        borderBottom={"1px solid #04041a"}
-                      >
-                        <Table.Cell>{product.name}</Table.Cell>
-                        <Table.Cell>${product.price.toFixed(2)}</Table.Cell>
+          <Table.Header
+            bg="primary"
+            borderBottom={"1px solid #04041a"}
+            height={"35px"}
+            fontSize={"xl"}
+          >
+            <Table.Row>
+              <Table.ColumnHeader>Factura</Table.ColumnHeader>
+              <Table.ColumnHeader>Fecha de Compra</Table.ColumnHeader>
+              <Table.ColumnHeader>Monto</Table.ColumnHeader>
+              <Table.ColumnHeader>Productos</Table.ColumnHeader>               
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {buys.map((buy, index) => (
+              <Table.Row
+                key={buy.idBuy}
+                sx={{
+                  backgroundColor: index % 2 === 0 ? "#ffe0b2" : "#f2f2b2",
+                  borderBottom: "1px solid  #04041a",
+                }}
+              >
+                <Table.Cell>{buy.idBuy}</Table.Cell>
+                <Table.Cell>
+                  {new Date(buy.buyDate).toLocaleDateString()}
+                </Table.Cell>
+                <Table.Cell>${buy.amount.toFixed(2)}</Table.Cell>
+                <Table.Cell>
+                  <Table.Root>
+                    <Table.Header
+                      bg="primary"
+                      borderBottom={"1px solid #04041a"}
+                    >
+                      <Table.Row>
+                        <Table.ColumnHeader>Producto</Table.ColumnHeader>
+                        <Table.ColumnHeader>Precio</Table.ColumnHeader>
                       </Table.Row>
-                    ))}
-                  </Table.Body>
-                </Table.Root>
+                    </Table.Header>
+                    <Table.Body>
+                      {buy.purchasedProducts.map((product) => (
+                        <Table.Row
+                          key={product.idProduct}
+                          borderBottom={"1px solid #04041a"}
+                        >
+                          <Table.Cell>{product.name}</Table.Cell>
+                          <Table.Cell>${product.price.toFixed(2)}</Table.Cell>
+                        </Table.Row>
+                      ))}
+                    </Table.Body>
+                  </Table.Root>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+          <Table.Footer
+            bg="primary"
+            textAlign={"center"}
+            height={"40px"}
+            borderTop={"1px solid #04041a"}
+            fontSize={"xl"}
+          >
+            <Table.Row>
+              <Table.Cell colSpan={3}>
+                Total de compras: {buys.length}
               </Table.Cell>
             </Table.Row>
-          ))}
-        </Table.Body>
-        <Table.Footer
-          bg="primary"
-          textAlign={"center"}
-          height={"40px"}
-          borderTop={"1px solid #04041a"}
-          fontSize={"xl"}
-        >
-          <Table.Row>
-            <Table.Cell colSpan={3}>Total de compras: {buys.length}</Table.Cell>
-          </Table.Row>
-        </Table.Footer>
-      </Table.Root>
+          </Table.Footer>
+        </Table.Root>
+      </Table.ScrollArea>
     </Box>
   );
 };
